2d43c4cf4f2396ddb9ce910ff7151f6842496d4e
phplpsummer <sh052100@naver.com>
2021-12-22 15:59:24 +0900

    좌우 버튼 이벤트 연결

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import './css/style.css';
 import {Logo, Menu} from './components/Header.js';
 import Footer from './components/Footer.js';
 import Panels from './components/Panels.js';
+import { useState } from 'react';
 
-
+const deg = 45;
 
 function App() {
+  let [num,setNum] = useState(0);
+
+  function plus(e){
+    setNum(++num);
+    const frame = e.currentTarget.closest("figure").querySelector("section");
+    frame.style.transform = `rotate(${45*num}deg)`;
+  }
+
+  function minus(e){
+    setNum(--num);
+    const frame = e.currentTarget.closest("figure").querySelector("section");
+    frame.style.transform = `rotate(${45*num}deg)`;
+  }
+
   return (
     <div className="App">
       <figure>
@@ -13,6 +28,9 @@ function App() {
         <Menu />
 
         <Panels />
+
+        <div className="btnPrev" onClick={(e)=>plus(e)}>Prev</div>
+        <div className="btnNext" onClick={(e)=>minus(e)}>Next</div>
 
         <Footer />
       </figure>
--- src/components/Panels.js
+++ src/components/Panels.js
@@ -8,18 +8,9 @@ const path = process.env.PUBLIC_URL;
 function Panels(){
   let [names, setNames] = useState(arr);
 
-  const changeState = ()=>{
-    //기존의 전개 연산자를 이용해서 새로운 배열로 복사
-    let newArr = [...arr];
-    //복사된 새로운 배열의 첫번째 값을 변경
-    newArr[0] = 'Escape';
-    //setNames라는 state변경 전용함수로, 위에서 복사-변경한 배열값으로 기존 state값을 바꿔치기
-    setNames(newArr);
-  }
 
   return (    
     <>
-      <button onClick={changeState}>button</button>
       <section>
       {
         names.map((data,index)=>{
